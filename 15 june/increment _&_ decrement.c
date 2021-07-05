f4974fd3adf1d9df08ee242b0006ce3c63190a79
Himanshu kumar <43819686+hksingh8228@users.noreply.github.com>
2021-07-05 15:20:43 +0530

    Update increment _&_ decrement.c

--- 15 june/increment _&_ decrement.c
+++ 15 june/increment _&_ decrement.c
@@ -1,1 +1,26 @@
-increment_
+Execute the line and find the output a = 2, b =1, x=(a++ && (!b) && b|| ++a);
+#include<stdio.h>
+int main()
+{
+	long int num;
+	int sum=0,rem;
+
+	printf("enter a number: ");
+	scanf("%ld",&num);
+
+	while(num/10 !=0)
+	{ 
+		sum=0;
+		while(num!=0)
+		{
+			rem= num%10;
+			sum =+ 10;
+			num = num/10;
+		}
+		num = sum;
+	}
+	printf("sum= %d",sum);
+
+	return 0;
+}
+
